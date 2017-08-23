aa97c6a2926724468f423cc6e7a4f94be17ad3a4
janherich <jan.herich@gmail.com>
2017-08-24 01:10:36 +0200

    Fix mysterious bug

--- src/status_im/bots/models.cljs
+++ src/status_im/bots/models.cljs
@@ -1,15 +1,15 @@
 (ns status-im.bots.models)
 
 (defn set-in-bot-db
-  [{:keys [current-chat-id] :as db} {:keys [bot path value]}]
+  [{:keys [current-chat-id] :as app-db} {:keys [bot path value]}]
   (let [bot (or bot current-chat-id)]
-    (assoc-in db (concat [:bot-db bot] path) value)))
+    (assoc-in app-db (concat [:bot-db bot] path) value)))
 
 (defn update-bot-db
-  [{:keys [current-chat-id] :as db} {:keys [bot db]}]
+  [{:keys [current-chat-id] :as app-db} {:keys [bot db]}]
   (let [bot (or bot current-chat-id)]
-    (update-in db [:bot-db bot] merge db)))
+    (update-in app-db [:bot-db bot] merge db)))
 
 (defn clear-bot-db
-  [{:keys [current-chat-id] :as db}]
-  (assoc-in db [:bot-db current-chat-id] nil))
+  [{:keys [current-chat-id] :as app-db}]
+  (assoc-in app-db [:bot-db current-chat-id] nil))
